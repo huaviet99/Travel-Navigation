ff4497dd3fc0fda9afd4776a1544ffe4e0220f08
Viet Hua <huaviet999@gmail.com>
2022-12-04 15:53:20 +0700

    Setup-github-action

---
+++ ci-cd/ios-unit-test-script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Arguments
+scheme=$1
+
+# Find iOS Simulator devices
+device=`xcrun xctrace list devices 2>&1 | grep -oE 'iPhone.*?[^\(]+' | head -1 | awk '{$1=$1;print}' | sed -e "s/ Simulator$//"`
+platform="iOS Simulator"
+destination="platform=$platform,name=$device"
+
+echo "Current scheme: TravelNavigation"
+echo "Start building with destination: $destination"
+
+# Build application
+set -o pipefail && xcodebuild build-for-testing \
+    -workspace TravelNavigation.xcworkspace \
+    -scheme "$scheme" \
+    -destination "$destination" \
+    -quiet
+
+echo "Build successful"
+echo "Start testing"
+
+# Start running tests
+set -o pipefail && xcodebuild test-without-building \
+    -workspace TravelNavigation.xcworkspace \
+    -scheme "$scheme" \
+    -sdk iphonesimulator \
+    -destination "$destination" \
+    -quiet
+echo "Test successful"
